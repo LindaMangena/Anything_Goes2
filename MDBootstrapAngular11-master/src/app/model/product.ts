c94ac9f5be16f2bdd92252f509d8731fa1e2fa1c
hlatsengwaile@gmail.com <hlatsengwaile@gmail.com>
2021-06-05 12:18:53 +0200

    added mock

---
+++ MDBootstrapAngular11-master/src/app/model/product.ts
@@ -0,0 +1,8 @@
+import { TooltipConfig } from 'angular-bootstrap-md';
+
+
+export interface Product {
+    name: string;
+    id: Number;
+    level:string;
+}
---
+++ MDBootstrapAngular11-master/src/app/productMock.ts
@@ -0,0 +1,54 @@
+import {    Product} from './model/Product'
+export let Products: Product[] =[
+{
+    name: 'Drill',
+    id: 1,
+    level:' tool',
+},
+{
+    name: 'Drill',
+    id: 1,
+    level: ' tool',
+},
+{
+    name: 'Drill',
+    id: 1,
+    level: ' tool',
+},
+{
+    name: 'Drill',
+    id: 1,
+    level: ' tool',
+},
+{
+    name: 'Drill',
+    id: 1,
+    level: ' tool',
+},
+{
+    name: 'Drill',
+    id: 1,
+    level: ' tool',
+},
+{
+    name: 'Drill',
+    id: 1,
+    level: ' tool',
+},
+{
+    name: 'Drill',
+    id: 1,
+    level: ' tool',
+},
+{
+    name: 'Drill',
+    id: 1,
+    level: ' tool',
+},
+{
+    name: 'Drill',
+    id: 1,
+    level: ' tool',
+}
+
+];
